481f2e583a330cc8f8674d733861db167277b17e
Tim Gallaugher <tim.gallaugher@gmail.com>
2022-05-05 12:58:37 -0400

    add cancel button

--- resources/js/NexusStatamicBardFootnotesMenu.vue
+++ resources/js/NexusStatamicBardFootnotesMenu.vue
@@ -12,19 +12,26 @@
         <div class="footnote-container" v-if="showOptions" v-click-outside="closeFootnoteMenu">
           <div class="px-2 py-2 border-b">
             <div class="h-8 mb-2 p-1 border rounded border-grey-50 flex items-center">
-              <input type='text' id="url" v-model.lazy='url' placeholder="URL" class="footnote-input input h-auto text-sm"/>
+              <input type='text' v-model.lazy='url' placeholder="URL" class="footnote-input input h-auto text-sm"/>
             </div>
             <div class="h-8 mb-2 p-1 border rounded border-grey-50 flex items-center">
-              <input type='text' name="text" v-model.lazy='text' placeholder="Text (Optional)" class="footnote-input input h-auto text-sm"/>
+              <input type='text' v-model.lazy='text' placeholder="Text (Optional)" class="footnote-input input h-auto text-sm"/>
             </div>
           </div>
           <div class="flex items-center justify-end space-x-1 font-normal px-2 py-1.5">
             <button
-                aria-label="Set Footnote"
+                aria-label="Cancel Changes"
                 class="btn btn-sm has-tooltip"
                 data-original-title="null"
-                @click="closeFootnoteMenu">
-              OK
+                @click="cancelChanges">
+              Cancel
+            </button>
+            <button
+                aria-label="Save Changes"
+                class="btn btn-sm has-tooltip"
+                data-original-title="null"
+                @click="saveChanges">
+              Save
             </button>
           </div>
         </div>
@@ -52,24 +59,32 @@ export default {
         };
     },
     methods: {
-        updateShowOptions() {
-            this.showOptions = !this.showOptions;
-            this.url = this.currentUrl;
-            this.text = this.currentText;
-        },
-        closeFootnoteMenu() {
-            //make sure data is saved
-            this.setFootnote();
-            // close the menu
-            this.showOptions = false;
-        },
-        setFootnote() {
-            // update the editor
-            this.editor.commands.nexusStatamicBardFootnote({
-                url: this.url,
-                text: this.text,
-            })
-        },
+      cancelChanges() {
+        // close the menu
+        this.closeFootnoteMenu();
+      },
+      closeFootnoteMenu() {
+        // close the menu
+        this.showOptions = false;
+      },
+      saveChanges() {
+        //make sure data is saved
+        this.setFootnote();
+        // close the menu
+        this.closeFootnoteMenu();
+      },
+      setFootnote() {
+        // update the editor
+        this.editor.commands.nexusStatamicBardFootnote({
+          url: this.url,
+          text: this.text,
+        })
+      },
+      updateShowOptions() {
+        this.showOptions = !this.showOptions;
+        this.url = this.currentUrl;
+        this.text = this.currentText;
+      },
     }
 };
 </script>
